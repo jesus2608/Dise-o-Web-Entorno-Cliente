f4cac885a38242dc6198ed7ac4102901a618fb2a
jesus <jmoral.jj@gmail.com>
2024-09-24 09:36:28 +0200

    Actividades

---
+++ Actividades/Ejercicio 2/SegundoJesusMoral.js
@@ -0,0 +1,3 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido=prompt("Ingrese su apellido");
+alert("Bienvenido a nuestra pagina web "+nombre+" "+apellido);
---
+++ Actividades/Ejercicio 4/JesusMoral4.js
@@ -0,0 +1,3 @@
+let numero1= prompt("Numero 1: ");
+let numeroTriple= numero1*3;
+console.log("El resultado del triple de este numero es: "+numeroTriple);
