d92267b39b818902bd84ca6b8e32779225748d31
jesus <jmoral.jj@gmail.com>
2024-09-26 08:49:09 +0200

    Ejercicios2.2

---
+++ Actividades2.2/JesusMoral6.js
@@ -0,0 +1,19 @@
+//let x = 30 let y = "a" y === "b" || x >= 30
+//true
+//let x = 15 
+//let y = 2 
+//!( x == "15" || x === y ) && ! (y !== 2 && x <= y) false
+
+var x= "Juan";
+var y= "Juan";
+console.log(x==y);
+
+var y ="Miguel";
+console.log(x==y);
+
+var y = 1;
+console.log(x==y);
+
+var x = "1";
+console.log(x==y);
+
